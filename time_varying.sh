2bcf3456b2e49fe55ea09c7089d8d06b0835a6ba
sprack <tp1515@ic.ac.uk>
2019-01-23 23:55:26 +0000

    scaling max iter

---
+++ time_varying.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# usage: ./time_vary [parameter to vary] [source]
+
+vary=$1
+BIN=$2
+
+for iter in 1 128 512 1024 2048 4096; do
+    eval "{ time ${BIN} -${vary} ${iter} > /dev/null; } 2>&1 |& grep real"
+    eval "{ time ${BIN} -${vary} ${iter} -engine parallel_inner > /dev/null; } 2>&1 |& grep real"
+    eval "{ time ${BIN} -${vary} ${iter} -engine parallel_outer > /dev/null; } 2>&1 |& grep real"
+    eval "{ time ${BIN} -${vary} ${iter} -engine parallel_both > /dev/null; } 2>&1 |& grep real"
+    echo "${vary} = ${iter}"
+    echo
+done
