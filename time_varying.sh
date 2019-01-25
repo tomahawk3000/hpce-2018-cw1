75c83568ebc520d4b877c627a23823c8df9b652b
sprack <tp1515@ic.ac.uk>
2019-01-25 20:00:45 +0000

    scaling others

--- time_varying.sh
+++ time_varying.sh
@@ -5,7 +5,7 @@
 vary=$1
 BIN=$2
 
-for iter in 1 128 512 1024 2048 4096; do
+for iter in 1 32 64 128 256 512; do
     eval "{ time ${BIN} -${vary} ${iter} > /dev/null; } 2>&1 |& grep real"
     eval "{ time ${BIN} -${vary} ${iter} -engine parallel_inner > /dev/null; } 2>&1 |& grep real"
     eval "{ time ${BIN} -${vary} ${iter} -engine parallel_outer > /dev/null; } 2>&1 |& grep real"
